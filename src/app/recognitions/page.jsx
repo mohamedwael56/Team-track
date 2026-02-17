5685b82907725979b5e804c57af89b389f1682f2
mohamed wael <mohmedhomos0147@gmail.com>
2026-02-17 16:40:48 +0200

    create the recognitions page

---
+++ src/app/recognitions/page.jsx
@@ -0,0 +1,193 @@
+import Header from '@/components/header'
+import Sidebar from '@/components/sidebar'
+import React from 'react'
+import Link from 'next/link'
+
+function page() {
+  return (
+    <div className='flex'>
+<div className='flex-1 ml-69'>
+<Header />
+<main>
+<div className='flex flex-col gap-5 m-5 bg-gray-100 p-5 rounded-2xl'>
+<div className='flex gap-3 items-center'>
+<Link href="/home" className="text-blue-900 text-sm hover:text-blue-700 transition duration-300">Back</Link>
+<p className='text-black text-xl font-bold'>Recognitions</p>
+</div>
+
+<hr />
+
+<div className='grid grid-cols-3 gap-3'>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+       <div className="flex justify-center bg-orange-100 py-2 px-3 rounded-xl gap-3 mt-3">
+
+<div className="flex flex-col w-full items-center">
+<h1 className=" font-bold text-xl text-amber-500 items-start"> mr.punctuality</h1>
+  <img src="/icons/rectangle.png" alt="" />
+
+<img src="/icons/award.png" alt="" />
+<div className="flex text-zinc-800 flex-row mt-2 items-center gap-2">
+</div>
+<div className='flex w-full text-black justify-between'>
+  <div className='flex items-center gap-2'>
+  <img src="/icons/vector2.png" alt="" />
+<span>+20</span>
+</div>
+<span className="ml-2">Tue 27 Aug 2024</span>
+</div>
+</div>
+</div>
+</div>
+
+  </div>
+</main>
+</div>
+<Sidebar   />
+    </div>
+  )
+}
+
+export default page
