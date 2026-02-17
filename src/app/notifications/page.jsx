ead7dda9a96bd8558e70bd9406c37feccfcbb955
mohamed wael <mohmedhomos0147@gmail.com>
2026-02-17 16:41:44 +0200

    create the notifications pages and editing the general css file

---
+++ src/app/notifications/page.jsx
@@ -0,0 +1,134 @@
+import Sidebar from '@/components/sidebar'
+import React from 'react'
+import Header from '@/components/header'
+function page() {
+  return (
+    <div className='flex'>
+        <div className='flex-1 ml-69'>
+        <Header />
+        <main>
+            <div className='p-5 flex flex-col bg-gray-100 rounded-2xl '>
+            <div className='mb-4 text-black text-xl'> 
+        notifications
+            </div>
+              <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+            <div className="flex my-5 items-center gap-2">
+            <div>
+                <img src="/icons/alert-outline.svg" alt="" />
+            </div>
+            <div className='flex flex-col'>
+            <h1 className='text-black'>UI Task less than 8 days</h1>
+            <p className='text-gray-300'>Phillip, your assignment is less than 8 days away from reaching</p>
+            </div>
+            </div>
+            <hr />
+          
+            </div>
+        </main>
+        </div>
+        <Sidebar />
+    </div>
+  )
+}
+
+export default page
