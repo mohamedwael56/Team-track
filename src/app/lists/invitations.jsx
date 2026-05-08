2f2332a0c92ff9529f3554af896e3816b8ad9d7b
mohamed wael <mohmedhomos0147@gmail.com>
2026-05-08 10:38:53 +0300

    looped through the invitations page to make the code more clean

--- src/app/lists/invitations.jsx
+++ src/app/lists/invitations.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import Link from 'next/link'
 import { useState } from 'react';
 function Invitations() {
+    const invitationRequests=[
+        {
+            id:1,
+            name:'mohamed ahmed',
+            avatar:'/mohamed.png',
+            time:'9 days ago',
+        },
+        {
+            id:2,
+            name:'ahmed mohamed',
+            avatar:'/mohamed.png',
+            time:'5 days ago',
+        },
+        {
+            id:3,
+            name:'mohamed ali',
+            avatar:'/mohamed.png',
+            time:'3 days ago',
+        },
+    ]
     const [openMessage, setOpenMessage] = useState(false);
   return (
     <>
@@ -25,15 +45,18 @@ openMessage&&(
     }
     
 <div className="grid grid-cols-3 gap-5 p-5">
-    
-    <div className="border text-xs shadow-sm flex flex-col rounded-2xl p-5">
+    {
+        invitationRequests.map((request)=>{
+            return(
+                <>
+                  <div className="border text-xs shadow-sm flex flex-col rounded-2xl p-5">
 <div className="flex justify-between items-center">
     <div className="flex gap-2 flex-row items-center">
         <img src="/profile/view-circle.png" className='w-5' alt="" />
-        <img src="/mohamed.png" className='w-8' alt="" />
-        <p className='text-black font-bold'>mohamed ahmed</p>
+        <img src={request.avatar} className='w-8' alt="" />
+        <p className='text-black font-bold'>{request.name}</p>
     </div>
-    <div className="text-gray-400">7 days ago</div>
+    <div className="text-gray-400">{request.time}</div>
 </div>
 <div className="flex gap-1 my-2">
     <p className='text-gray-400'>Sent you a collaboration invite to</p>
@@ -44,42 +67,11 @@ openMessage&&(
     <button className='flex-1 bg-blue-900 rounded-2xl cursor-pointer py-2 px-5 text-white'>accept</button>
 </div>
 </div>
-    <div className="border text-xs shadow-sm flex flex-col rounded-2xl p-5">
-<div className="flex justify-between items-center">
-    <div className="flex gap-2 flex-row items-center">
-        <img src="/profile/view-circle.png" className='w-5' alt="" />
-        <img src="/mohamed.png" className='w-8' alt="" />
-        <p className='text-black font-bold'>mohamed ahmed</p>
-    </div>
-    <div className="text-gray-400">7 days ago</div>
-</div>
-<div className="flex gap-1 my-2">
-    <p className='text-gray-400'>Sent you a collaboration invite to</p>
-    <Link href='/lists/lists-invitations' className='font-bold text-blue-500'> My List</Link>
-</div>
-<div className="flex gap-2">
-    <button className='flex-1 bg-red-200 rounded-2xl text-red-500 py-2 px-5 cursor-pointer'>reject</button>
-    <button className='flex-1 bg-blue-900 rounded-2xl cursor-pointer py-2 px-5 text-white'>accept</button>
-</div>
-</div>
-    <div className="border text-xs shadow-sm flex flex-col rounded-2xl p-5">
-<div className="flex justify-between items-center">
-    <div className="flex gap-2 flex-row items-center">
-        <img src="/profile/view-circle.png" className='w-5' alt="" />
-        <img src="/mohamed.png" className='w-8' alt="" />
-        <p className='text-black font-bold'>mohamed ahmed</p>
-    </div>
-    <div className="text-gray-400">7 days ago</div>
-</div>
-<div className="flex gap-1 my-2">
-    <p className='text-gray-400'>Sent you a collaboration invite to</p>
-    <Link href='/lists/lists-invitations' className='font-bold text-blue-500'> My List</Link>
-</div>
-<div className="flex gap-2">
-    <button className='flex-1 bg-red-200 rounded-2xl text-red-500 py-2 px-5 cursor-pointer'>reject</button>
-    <button className='flex-1 bg-blue-900 rounded-2xl cursor-pointer py-2 px-5 text-white'>accept</button>
-</div>
-</div>
+                </>
+            )
+    })}
+  
+
 </div> </> )
 }
 
